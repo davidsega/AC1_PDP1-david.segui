7008ef8bc6ffbf1ccb809ae8a25685f24bacd1f3
MSI-David <david.segui@students.salle.url.edu>
2018-10-02 13:01:38 +0200

    feat: S'ha omplert el fitxer LlistaBIDord.h amb totes les capÃaleres de les funcions que s'implementaran en el fitxer LlistaBIDord.h.La llista es tracta d'una llista d'enters ordenada de menor a major
    Les funcions que s'hauran d'implementar son les seguents:
    - Crear llista: Crea la llista bid amb 2 nodes que actuen com fantasma
    - Inserir element: Insereix un element a la llista de manera ordenada
    - Consulta: Consulta l'element on es troba el pdi
    - Esborra: Esborra l'element on es troba el pdi
    - Avansa: Avansa un element en la llista
    - Retrocedeix: Retrocedeix un element a la llista
    - VesInici: Mou el pdi al inici de la llista
    - VesFinal: Mou el pdi al final de la llista
    - Fi: Comprova si s'ha arribat al final de la llista
    - Inici: Comprova si el pdi es troba al inici de la llista
    - Buida: Comprova si la llista es troba buida
    - Destrueix: Destrueix la llista

---
+++ LlistaBIDord.h
@@ -0,0 +1,34 @@
+#ifndef _LLISTABIDORD_H
+
+	#define _LLISTABIDORD_H
+
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+
+
+  typedef struct N{
+    int e;
+    struct N* seg;
+    struct N* ant;
+  }Node;
+
+  typedef struct{
+    Node* pri;
+    Node* ult;
+    Node* pdi;
+  }LlistaBIDord;
+
+LlistaBIDord LlistaBIDord_Crea();
+void LlistaBIDord_Insereix(LlistaBIDord*l,int e);
+int LlistaBIDord_Consulta(LlistaBIDord l);
+void LlistaBIDord_Esborra(LlistaBIDord*l);
+void LlistaBIDord_Avansa(LlistaBIDord*l);
+void LlistaBIDord_Retrocedeix(LlistaBIDord*l);
+void LlistaBIDord_VesInici(LlistaBIDord*l);
+void LlistaBIDord_VesFi(LlistaBIDord*l);
+void LlistaBIDord_Inici(LlistaBIDord*l);
+void LlistaBIDord_Fi(LlistaBIDord*l);
+int LlistaBDIord_Buida(LlistaBIDord l);
+void LlistaBIDord_Destrueix(LlistaBIDord* l);
+#endif
