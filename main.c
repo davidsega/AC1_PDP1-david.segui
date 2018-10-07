2cce4bda225b6a7ea7dbf59d7ddbc2a86836778b
MSI-David <david.segui@students.salle.url.edu>
2018-10-07 13:21:40 +0200

    feat: S'ha escrit el programa main.c que es el programa que s'utilitzara per debugar el codi i demostrar el correcte funcionament de la LlistaBIDord.
    El programa funciona de la segÃent manera, crea una llistaBIDord,
    insereix 4 elements desordenats, n'elimina 1, es situa al inici,
    recorre tota la llista mostant cadascun dels elements i despres
    la reccorre del reves. Finalment destrueix la llista i comprova
    si aquesta es troba buida

--- main.c
+++ main.c
@@ -2,7 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "LlistaBIDord.h"
+
 int main (int argc,char* argv[]){
-  printf("hola\n");
+  int i = 0;
+  LlistaBIDord l;
+  l= LlistaBIDord_Crea();
+  LlistaBIDord_Insereix(&l,1);
+  LlistaBIDord_Insereix(&l,10);
+  LlistaBIDord_Insereix(&l,4);
+  LlistaBIDord_Insereix(&l,8);
+  LlistaBIDord_VesInici(&l);
+  printf("Inici de la llista\n", );
+  LlistaBDIord_Esborra(&l);
+  printf("element esborrat: %d\n",LlistaBIDord_Consulta(l));
+  while(LlistaBIDord_Fi(l) != 1){
+    LlistaBIDord_Avansa(&l);
+    printf("avansa llista\nelement: %d\n",LlistaBIDord_Consulta(l));
+  }
+  LlistaBDIord_vesFi(&l);
+  while(LlistaBIDord_Inici(l) != 1){
+    LlistaBIDord_Retrocedeix(&l);
+    printf("retrocedeix llista\nelement: %d\n",LlistaBIDord_Consulta(l));
+  }
+  LlistaBIDord_Destrueix(&l);
+  if(LlistaBIDord_Buida(l)){
+    printf("Procediment correcte\n", );
+  }else{
+    printf("Error en l'eliminació de la llista\n", );
+  }
   return 0;
 }
